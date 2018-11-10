a18fd45dca3c7d7ea2e62bfdffa9bcc0bc1c1592
Kalana <edata.incore@gmail.com>
2018-11-10 11:50:36 +0530

    intial

---
+++ Main.py
@@ -0,0 +1,14 @@
+# Kalana Dhanajaya Rathnayake 
+# University of Peradeniya 
+# Faculty of Engineering 
+
+# Text Classification using Machine Learning
+
+
+# import  lib 
+
+
+# import file
+
+print("main")
+
